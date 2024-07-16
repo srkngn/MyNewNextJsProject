76a45a9759186687ab8342606439e25aed147fd7
srkngn <serkangn3649@gmail.com>
2024-07-16 17:00:56 +0300

    customer page closed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -3,19 +3,19 @@ import { CustomerField, FormattedCustomersTable } from "@/app/lib/definitions";
 import CustomersTable from "@/app/ui/customers/table";
 
 export default async function Page () {
-    const customers: CustomerField[] = await fetchCustomers();
+  //   const customers: CustomerField[] = await fetchCustomers();
 
-  // Müşteri verilerini uygun formata dönüştür
-  const formattedCustomers: FormattedCustomersTable[] = customers.map((customer: CustomerField) => ({
-    id: customer.id,
-    name:customer.name,
-    email: customer.email,
-    image_url: '/customers/evil-rabbit.png',
-    phone_number: customer.phone_number,
-    total_pending: "0",
-    total_paid: false
-  }));
-    return <main>
-        <CustomersTable customers={formattedCustomers}/>
-    </main>
+  // // Müşteri verilerini uygun formata dönüştür
+  // const formattedCustomers: FormattedCustomersTable[] = customers.map((customer: CustomerField) => ({
+  //   id: customer.id,
+  //   name:customer.name,
+  //   email: customer.email,
+  //   image_url: '/customers/evil-rabbit.png',
+  //   phone_number: customer.phone_number,
+  //   total_pending: "0",
+  //   total_paid: false
+  // }));
+  //   return <main>
+  //       <CustomersTable customers={formattedCustomers}/>
+  //   </main>
 }
