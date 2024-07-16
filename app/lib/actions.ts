324d67f712c74e0450cde473e26ec6cc8e55c4e1
srkngn <serkangn3649@gmail.com>
2024-07-16 17:10:41 +0300

    auth removed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,24 +7,24 @@ import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 import email from 'next-auth/providers/email';
 
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData,
-) {
-  try {
-    await signIn('credentials', formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
-    }
-    throw error;
-  }
-}
+// export async function authenticate(
+//   prevState: string | undefined,
+//   formData: FormData,
+// ) {
+//   try {
+//     await signIn('credentials', formData);
+//   } catch (error) {
+//     if (error instanceof AuthError) {
+//       switch (error.type) {
+//         case 'CredentialsSignin':
+//           return 'Invalid credentials.';
+//         default:
+//           return 'Something went wrong.';
+//       }
+//     }
+//     throw error;
+//   }
+// }
 
  
 const FormSchema = z.object({
