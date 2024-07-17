58770a6afc1006a62df381ba16139b5b9a7ad29f
srkngn <serkangn3649@gmail.com>
2024-07-17 10:25:38 +0300

    refaclogin

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,4 @@
-'use client';
+/* 'use client';
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -77,4 +77,4 @@ export default function LoginForm() {
     </form>
   );
 }
- 
+  */
