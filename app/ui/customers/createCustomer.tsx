c75ff66fcac183ef2a5a8c417ff7acd1ae398d2f
srkngn <serkangn3649@gmail.com>
2024-08-07 15:59:17 +0300

    css edited6

--- app/ui/customers/createCustomer.tsx
+++ app/ui/customers/createCustomer.tsx
@@ -85,12 +85,6 @@ export default function Form() {
         </div>
             </div>
             <div className="mt-6 flex justify-end gap-4">
-        <Link
-          href="/dashboard/invoices"
-          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
-        >
-          Cancel
-        </Link>
         <Button type="submit">Create Customer</Button>
       </div>
       </form>
